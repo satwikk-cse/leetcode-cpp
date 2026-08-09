4bd910c2d3cdf4b6db32a8364604edcb791a1db1
Satwik Mishra <satwikmishracse@gmail.com>
2026-08-09 22:52:46 +0530

    Add next permutation logic to solution
    Implemented next permutation algorithm with detailed steps.

--- next-permutation/solution.cpp
+++ next-permutation/solution.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
+        // Step-1: Finding pivot index
+        int idx=-1;
+        for(int i=nums.size()-2;i>=0;i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                idx=i;      // Pivot index
+                break;
+            }
+        }
+
+        if(idx==-1)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        
+        // Step-2: Sorting/Reverse after pivot
+        reverse(nums.begin()+idx+1,nums.end());
+
+        //Step-3 Finding just greater element that idx
+        int j=-1;
+        for(int i=idx+1;i<nums.size();i++)
+        {
+            if(nums[i]>nums[idx])
+            {
+                j=i;
+                break;
+            }
+        }
+
+        // Step-4: Swapping nums[idx] and nums[j]
+        swap(nums[idx],nums[j]);
     }
 };
