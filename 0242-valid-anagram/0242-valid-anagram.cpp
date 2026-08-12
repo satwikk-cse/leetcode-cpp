5b81e23376eb3167c96049d85ee7a9cd1e5780d0
Satwik Mishra <satwikmishracse@gmail.com>
2026-08-12 23:03:05 +0530

    Time: 0 ms (100%), Space: 9.9 MB (11.6%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // sort(s.begin(),s.end());
-        // sort(t.begin(),t.end());
-        // if(s==t) return true;
-        // return false;
 
-        if(s.size()!=t.size()) return false;
+        // Approach-1
+
+        /*sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t) return true;
+        return false;*/
+
+
+        // Approach-2
+
+        /*if(s.size()!=t.size()) return false;
         unordered_map<char, int>mp1;
         for(int i=0;i<s.size();i++)
         {
@@ -32,6 +38,29 @@ public:
             }
             else return false;
         }
+        return true;*/
+
+        // Approach-3
+
+        if(s.size()!=t.size()) return false;
+        unordered_map<char, int>mp1;
+        for(int i=0;i<s.size();i++)
+        {
+            mp1[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++)
+        {
+            char ch=t[i];
+            if(mp1.find(ch) != mp1.end())
+            {
+                mp1[ch]--;
+                if(mp1[ch]==0) mp1.erase(ch);
+            }
+            else return false;
+        }
+
+        if(mp1.size()>0) return false;
         return true;
+
     }
 };
