1aaad205aa598fda21ce7e3ee2104ae957184a3f
Satwik Mishra <satwikmishracse@gmail.com>
2026-09-06 22:49:57 +0530

    Time: 1 ms (16.58%), Space: 9.1 MB (15.27%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char>s;
+        for(int i=0;i<sentence.size();i++)
+        {
+            s.insert(sentence[i]);
+        }
+        if(s.size()==26) return true;
+        return false;
+    }
+};
